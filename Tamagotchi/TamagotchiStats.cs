f6d6cbbc24677536a66df28cfd7ddfe0b4455105
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-06 10:27:28 +0200

    Init.

---
+++ Tamagotchi/TamagotchiStats.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+    public class TamagotchiStats
+    {
+        private int hunger = 0;
+        private int boredom = 0;
+        private List<string> words = new List<string>();
+    }
+}
