1942dc4d4604de99a0c8aada818937a37d2e00eb
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-07 11:10:04 +0200

    Work

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Tamagotchi
 {
@@ -6,7 +7,75 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Du kör nu Tamagotchi game");
+
+            Tamagotchi t1 = new Tamagotchi();
+            System.Console.WriteLine("Vad vill du döpa din Tamagotchi till?");
+            t1.name = Console.ReadLine();
+
+            System.Console.WriteLine("Din tamagotchi heter nu " + t1.name);
+            Thread.Sleep(2000);
+            Console.Clear();
+            
+
+            bool gameIsRunning = true;
+            while (gameIsRunning)
+            {
+                t1.PrintStats();
+                t1.GetAlive();
+                if (t1.GetAlive() == false)
+                {
+                    gameIsRunning = false;
+                }
+                else
+                {
+                    System.Console.WriteLine("Vad vill du att din tamagotchi ska göra?:");
+                    System.Console.WriteLine(@"
+                    [A] Lära den ett nytt ord
+                    [B] Hälsa på den
+                    [C] Mata den
+                    [D] Göra ingenting        
+                    ");
+                    string input = Console.ReadLine();
+                    input = input.ToUpper(); 
+
+                    bool correctInput = false;
+                    while (!correctInput)
+                    {
+                        if (input == "A")
+                        {
+                            t1.Teach();
+                            correctInput = true;
+                        } 
+                        else if (input == "B")
+                        {
+                            t1.Hi();
+                            correctInput = true;
+                        }
+                        else if (input == "C")
+                        {
+                            t1.Feed();
+                            System.Console.WriteLine("Du matade din tamagotchi");
+                            correctInput = true;
+                        }
+                        else if (input == "D")
+                        {
+                            correctInput = true;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Vänligen skriv en riktig input");
+                        }
+                    }
+                                      
+
+                    t1.Tick();
+                }
+            }
+
+            System.Console.WriteLine("GAME OVER");
+            Console.ReadLine();
+
         }
     }
 }
--- Tamagotchi/TamagotchiStats.cs
+++ Tamagotchi/TamagotchiStats.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 
 namespace Tamagotchi
 {
-    public class TamagotchiStats
+    public class Tamagotchi
     {
-        private int hunger = 0;
-        private int boredom = 0;
-        private List<string> words = new List<string>();
+        int hunger = 0;
+        int boredom = 0;
+        List<string> words = new List<string>();
+
+        bool isAlive;
+        Random generator = new Random();
+        public string name = "";
+
+        public void Feed()
+        {
+            // Feed() sänker Hunger
+            hunger -= 3;
+
+        }   
+
+        public void Hi()
+        {
+            // Hi() skriver ut ett slumpat ord från words, och anropar ReduceBoredom.
+            ReduceBoredom();
+        }
+
+        public void Teach()
+        {
+            // Teach(string word) lägger till ett ord i words, och anropar ReduceBoredom.
+            ReduceBoredom();
+        }
+
+        public void Tick()
+        {
+            // Tick() ökar hunger och boredom, och om någon av dem kommer över 10 så blir isAlive false.
+            boredom++;
+            hunger++;
+        }
+
+        public void PrintStats()
+        {
+            // PrintStats() skriver ut nuvarande hunger och bredom, och meddelar också huruvida tamagotchin lever.
+            if (hunger > 10 || boredom > 10)
+            {
+                System.Console.WriteLine("Din tamagotchi är död");
+            }
+            else
+            {
+                System.Console.WriteLine("Din tamagotchis stats lyder:");
+                System.Console.WriteLine(hunger + " hunger");
+                System.Console.WriteLine(boredom + " boredom");
+            }
+
+        }
+
+        public bool GetAlive()
+        {
+            if (hunger > 10 || boredom > 10)
+            {
+                return isAlive = false;
+            }
+            else
+            {
+                return isAlive = true;
+            }
+        }
+
+        void ReduceBoredom()
+        {
+            // ReduceBoredom() sänker boredom.
+            boredom -= 2;
+
+        }
+
+
+
     }
 }
